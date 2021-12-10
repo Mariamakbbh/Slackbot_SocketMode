7036643fda9385d2279ee2ee0885c9ec60183752
shipperizer <alexcabb@gmail.com>
2021-12-10 12:40:25 +0000

    fix: put config.go back into git

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,8 @@ import (
 )
 
 func Load() {
+	viper.BindEnv("slack.auth.token", "SLACK_AUTH_TOKEN")
+	viper.BindEnv("slack.app.token", "SLACK_APP_TOKEN")
 	viper.SetDefault("slack.auth.token", "dummy")
 	viper.SetDefault("slack.app.token", "dummy")
 	//viper.BindEnv("slack.channel", "#websocket-bot")
